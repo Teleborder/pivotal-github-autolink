1614d1bf76bae8859df7ea1a4417c612d5e83a9d
Trey Griffith <trey.griffith@gmail.com>
2015-03-18 16:01:38 -0700

    target blank

--- src/autolink.js
+++ src/autolink.js
@@ -1,4 +1,4 @@
 $(function() {
   nodesToReplace = $('.js-issue-title, .js-comment-body');
-  nodesToReplace.html(nodesToReplace.html().replace(/\[(?:Finishes|Delivers|Fixes) #(\d+)\]/gi, "<a href=\"https://www.pivotaltracker.com/story/show/$1\">$&</a>"));
+  nodesToReplace.html(nodesToReplace.html().replace(/\[(?:Finishes|Delivers|Fixes) #(\d+)\]/gi, "<a href=\"https://www.pivotaltracker.com/story/show/$1\" target=\"_blank\">$&</a>"));
 });
