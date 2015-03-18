252d44819497ed8ac5511c2da7cb371865b8d5b0
Trey Griffith <trey.griffith@gmail.com>
2015-03-18 16:02:41 -0700

    loop over each

--- src/autolink.js
+++ src/autolink.js
@@ -1,4 +1,6 @@
 $(function() {
   nodesToReplace = $('.js-issue-title, .js-comment-body');
-  nodesToReplace.html(nodesToReplace.html().replace(/\[(?:Finishes|Delivers|Fixes) #(\d+)\]/gi, "<a href=\"https://www.pivotaltracker.com/story/show/$1\" target=\"_blank\">$&</a>"));
+  nodesToReplace.each(function () {
+    $(this).html($(this).html().replace(/\[(?:Finishes|Delivers|Fixes) #(\d+)\]/gi, "<a href=\"https://www.pivotaltracker.com/story/show/$1\" target=\"_blank\">$&</a>"));
+  });
 });
