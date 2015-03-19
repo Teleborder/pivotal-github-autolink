ec8358f95cce407ce9ee4ffc436f2549a9c5bde6
Trey Griffith <trey.griffith@gmail.com>
2015-03-18 17:16:56 -0700

    recursively search nodes for instances of pivotal links

--- src/autolink.js
+++ src/autolink.js
@@ -1,6 +1,20 @@
 $(function() {
   nodesToReplace = $('.js-issue-title, .js-comment-body');
   nodesToReplace.each(function () {
-    $(this).html($(this).html().replace(/\[(?:Finishes|Delivers|Fixes) #(\d+)\]/gi, "<a href=\"https://www.pivotaltracker.com/story/show/$1\" target=\"_blank\">$&</a>"));
+    replacePivotalLinks(this);
   });
+
+  function replacePivotalLinks(node) {
+    $(node).contents().filter(function () {
+      if (this.nodeType === 1) {
+        // element nodeType
+        replacePivotalLinks(this);
+      } else if (this.nodeType === 3) {
+        // text nodeType
+        $(this).replaceWith(this.data.replace(/\[(?:Finishes|Delivers|Fixes) #(\d+)\]/gi, "<a href=\"https://www.pivotaltracker.com/story/show/$1\" target=\"_blank\">$&</a>"));
+      }
+    });
+  }
 });
+
+
